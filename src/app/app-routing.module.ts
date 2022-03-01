1ea8bb40bd235be3ad8510aabed5b47f443a4ebc
Matheus <matheus.mfa10@gmail.com>
2022-03-01 17:04:22 -0300

    @Matheus-Anjos [Task: alterações gerais ]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
@@ -11,7 +12,9 @@ const routes: Routes = [
 
   { path : 'cadastrar' , component: CadastrarComponent},
 
-  { path : 'sobre' , component: SobreComponent}
+  { path : 'sobre' , component: SobreComponent},
+
+  { path : 'inicio' , component: InicioComponent}
 ];
 
 
