89378a8b236ae9fa07e08e879393d92bccc78051
Thayrine <thayrinerenata@gmail.com>
2022-03-08 09:48:24 -0300

    @thayrinerenata [Task: Crud do Tema (erro na atualização do tema)]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -12,10 +12,16 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
   entrar(userLogin: UserLogin): Observable<UserLogin> {
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    //return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+
+    //Conexão com Heroku 
+   return this.http.post<UserLogin>('https://piredesocial.herokuapp.com/usuarios/logar', userLogin)
   }
   cadastrar(usuario: Usuario): Observable<Usuario> {
-    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
+    //return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
+
+    //Conexão com Heroku 
+    return this.http.post<Usuario>('https://piredesocial.herokuapp.com/usuarios/cadastrar', usuario)
   }
 
   logado() {
---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`https://piredesocial.herokuapp.com/temas/buscar/${id}`, this.token)
+
+    //Conexão local
+    //return this.http.get<Tema>(`http://localhost:8080/temas/buscar/${id}`, this.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+
+    return this.http.get<Tema[]>('https://piredesocial.herokuapp.com/temas/buscar', this.token)
+
+    //Conexão local
+    //return this.http.get<Tema[]>('http://localhost:8080/temas/buscar', this.token)
+  }
+
+  postTema( tema : Tema): Observable<Tema> {
+    return this.http.post<Tema>('https://piredesocial.herokuapp.com/temas/salvar', tema, this.token)
+
+    //Conexão local
+    //return this.http.post<Tema>('http://localhost:8080/temas/salvar', tema, this.token)
+  }
+
+  /* putTema(id: number): Observable<Tema> {
+    return this.http.put<Tema>(`https://piredesocial.herokuapp.com/temas/atualizar/${id}`, this.token) */
+
+    //Conexão local
+    //return this.http.put<Tema>('http://localhost:8080/temas/atualizar', tema, this.token)
+  //}
+  putTema(tema: Tema): Observable<Tema> {
+    return this.http.put<Tema>('https://piredesocial.herokuapp.com/temas/atualizar', tema, this.token);
+  }
+
+
+  deleteTema( id : number) {
+   return this.http.delete(`https://piredesocial.herokuapp.com/temas/apagar/${id}`, this.token )
+
+    //Conexão local
+    //return this.http.delete(`http://localhost:8080/temas/apagar/${id}`, this.token )
+  }
+
+
+
+
+
+
+
+}
