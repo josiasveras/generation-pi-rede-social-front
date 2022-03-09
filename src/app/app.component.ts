7443ae9303e06051d5a06107d2234ef703e0a4a3
josiasveras <josias_veras@hotmail.com>
2022-03-09 15:06:41 -0300

    @josiasveras [ Task: Corrigir bugs gerais ]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,19 @@ import { AuthService } from './service/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  sidebar: any
   constructor(
     public auth: AuthService
   ){}
+
+  menuCollapse() {
+    // let btn = document.querySelector("#btn");
+    this.sidebar = document.querySelector(".sidebar");
+    // let searchBtn = document.querySelector(".fa-search");
+
+    this.sidebar.classList.toggle("active");
+
+    // this.sidebar.classList.toggle("active");
+    // alert("Olá mundo!!");
+  }
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  sidebar: any;
+  // sidebar: any;
 
   constructor() { }
 
@@ -15,15 +15,15 @@ export class MenuComponent implements OnInit {
     window.scroll(0,0);
   }
 
-  menuCollapse() {
-    // let btn = document.querySelector("#btn");
-    this.sidebar = document.querySelector(".sidebar");
-    // let searchBtn = document.querySelector(".fa-search");
+  // menuCollapse() {
+  //   // let btn = document.querySelector("#btn");
+  //   this.sidebar = document.querySelector(".sidebar");
+  //   // let searchBtn = document.querySelector(".fa-search");
 
-    this.sidebar.classList.toggle("active");
+  //   this.sidebar.classList.toggle("active");
 
-    // this.sidebar.classList.toggle("active");
-    // alert("Olá mundo!!");
-  }
+  //   // this.sidebar.classList.toggle("active");
+  //   // alert("Olá mundo!!");
+  // }
 
 }
