73cef56f8cfa913038c412c1e4c499e8f2038724
josiasveras <josias_veras@hotmail.com>
2022-03-17 10:42:27 -0300

    @josiasveras [ Task: Alterações layout componente sobre ]

--- src/app/sobre/sobre.component.ts
+++ src/app/sobre/sobre.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-sobre',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    window.scroll(0, 0);
+
+    if (environment.token == '') {
+      alert('sua sessão expirou faça login novamente')
+      this.router.navigate(['/entrar'])
+    }
+    
   }
 
 }
